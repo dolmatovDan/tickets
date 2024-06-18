dedfd7ada1e93b2802d27ada645dfcbfd15bf68b
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2024-06-18 18:06:15 +0300

    correct polls

---
+++ assets/scripts/script_index.js
@@ -0,0 +1,31 @@
+start = sessionStorage.getItem('start');
+end = sessionStorage.getItem('end');
+day = sessionStorage.getItem('day')
+year = sessionStorage.getItem('year')
+month = sessionStorage.getItem('month')
+price = sessionStorage.getItem('price')
+train_type = sessionStorage.getItem('train_type')
+
+months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"]
+
+document.getElementById('path').innerHTML = start + " — " + end
+document.getElementById('date').innerHTML = day + " " + months[month - 1];
+document.getElementById('price').innerHTML = price + " ₽";
+
+if (train_type == "Электричка") {
+    document.getElementById('ticket_type').innerHTML = "Билет на электричку стандарт (обычный пригородный поезд)";
+    document.getElementById('train_image').src = "assets/img/train1.png";
+} else {
+    document.getElementById('ticket_type').innerHTML = "Билет на электричку «Ласточка»";
+    document.getElementById('train_image').src = "assets/img/train2.png";
+}
+
+const today = new Date(year, month, day)
+
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
+
+next_day = tomorrow.getDate()
+next_month = tomorrow.getMonth()
+
+document.getElementById("limits").innerHTML = "Действует до 01:00 " + String(next_day) + " " + months[next_month - 1]
---
+++ assets/scripts/script_menu.js
@@ -0,0 +1,25 @@
+const element = document.getElementById('data-button');
+element.addEventListener("click", function() {
+    start = document.getElementById('start').value
+    sessionStorage.setItem('start', start)
+
+    end = document.getElementById('end').value
+    sessionStorage.setItem('end', end)
+
+    day = document.getElementById('day').value
+    sessionStorage.setItem('day', day)
+
+    month = document.getElementById('month').value
+    sessionStorage.setItem('month', month)
+
+    year = document.getElementById('year').value
+    sessionStorage.setItem('year', year)
+
+    price = document.getElementById('price').value
+    sessionStorage.setItem('price', price)
+
+    option = document.getElementById('train_type')
+    train_type = option.options[option.selectedIndex].text
+
+    sessionStorage.setItem('train_type', train_type)
+});
